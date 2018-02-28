68b163e15373b4a81160be5164f89969dc28509e
Grant David Bachman <grantbachman@gmail.com>
2018-02-28 06:28:59 -0500

    Make app a python package

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def root():
-    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
